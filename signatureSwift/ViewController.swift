0d42b37371108f17c74c40be1bd3742343d6efb5
wenguoli <15160333779@163.com>
2016-09-19 10:58:45 +0800

    Initial Commit

---
+++ signatureSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  signatureSwift
+//
+//  Created by 温国力 on 16/9/19.
+//  Copyright © 2016年 wenguoli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
